de501983659ef9e9d889c0e3a1ce1a57fe39e22c
gorjanradevski <gorjanradevski@gmail.com>
2020-01-20 11:07:15 +0100

    deleteing the checkpoint

--- src/train_multimodal.py
+++ src/train_multimodal.py
@@ -65,21 +65,25 @@ def train(
     # Define training specifics
     config = BertConfig()
     config.vocab_size += len(visual2index)
-    model = nn.DataParallel(MultiModalBert(load_embeddings_path, config, device))
+    model = nn.DataParallel(MultiModalBert(load_embeddings_path, config, device)).to(
+        device
+    )
     # Loss and optimizer
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     cur_epoch = 0
     best_val_loss = sys.maxsize
     if checkpoint_path is not None:
-        checkpoint = torch.load(checkpoint_path)
+        checkpoint = torch.load(checkpoint_path, map_location=device)
         model.load_state_dict(checkpoint["model_state_dict"])
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
         cur_epoch = checkpoint["epoch"]
         best_val_loss = checkpoint["loss"]
+        # https://discuss.pytorch.org/t/cuda-out-of-memory-after-loading-model/50681
+        del checkpoint
+
         logger.warning(f"Starting training from checkpoint {checkpoint_path}")
 
-    model = model.to(device)
     for epoch in range(cur_epoch, epochs):
         logger.info(f"Starting epoch {epoch + 1}...")
         # Set model in train mode
