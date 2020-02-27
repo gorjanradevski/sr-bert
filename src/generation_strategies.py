b46f80021fbe7270fd0c937654c2254521df5b0b
gorjanradevski <gorjanradevski@gmail.com>
2020-02-27 15:04:23 +0100

    highest prob and lowest entropy fixed

--- src/generation_strategies.py
+++ src/generation_strategies.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+from torch.nn import functional as F
 
 from datasets import X_MASK, Y_MASK, F_MASK
 
@@ -141,6 +143,30 @@ def left_to_right_discrete(
     return x_ind, y_ind, f_ind
 
 
+def random_discrete(
+    ids_text, ids_vis, pos_text, x_ind, y_ind, f_ind, t_types, attn_mask, model
+):
+    # Set all indices to MASK tokens
+    x_ind[:, :] = X_MASK
+    y_ind[:, :] = Y_MASK
+    f_ind[:, :] = F_MASK
+    max_ids_text = ids_text.size()[1]
+    for _ in range(2):
+        indices = np.random.permutation(list(range(ids_vis.size()[1])))
+        for i in indices:
+            x_ind[:, i] = X_MASK
+            y_ind[:, i] = Y_MASK
+            f_ind[:, i] = F_MASK
+            x_scores, y_scores, f_scores = model(
+                ids_text, ids_vis, pos_text, x_ind, y_ind, f_ind, t_types, attn_mask
+            )
+            x_ind[:, i] = torch.argmax(x_scores, dim=-1)[:, max_ids_text:][:, i]
+            y_ind[:, i] = torch.argmax(y_scores, dim=-1)[:, max_ids_text:][:, i]
+            f_ind[:, i] = torch.argmax(f_scores, dim=-1)[:, max_ids_text:][:, i]
+
+    return x_ind, y_ind, f_ind
+
+
 def highest_probability(
     ids_text, ids_vis, pos_text, x_ind, y_ind, f_ind, t_types, attn_mask, model
 ):
@@ -150,6 +176,7 @@ def highest_probability(
     f_ind[:, :] = F_MASK
     batch_indices = list(range(ids_text.size()[0]))
     max_ids_text = ids_text.size()[1]
+
     predicted_indices_x = []
     predicted_indices_y = []
     predicted_indices_f = []
@@ -160,9 +187,9 @@ def highest_probability(
         )
         # If there are indices which are already chosen, change to a small number
         if len(predicted_indices_x) > 0:
-            x_scores[batch_indices, predicted_indices_x] = -1e15
-            y_scores[batch_indices, predicted_indices_y] = -1e15
-            f_scores[batch_indices, predicted_indices_f] = -1e15
+            x_scores[:, max_ids_text:][batch_indices, predicted_indices_x] = -1e15
+            y_scores[:, max_ids_text:][batch_indices, predicted_indices_y] = -1e15
+            f_scores[:, max_ids_text:][batch_indices, predicted_indices_f] = -1e15
 
         # Obtain the probabilities and the prediction for all elements
         prob_x, pred_x = torch.max(x_scores, dim=-1)
@@ -180,15 +207,15 @@ def highest_probability(
         predicted_indices_f.append(index_f.tolist())
 
         # Change the index with the max probability with its prediction
-        x_ind[batch_indices, index_x] = pred_x[batch_indices, index_x]
-        y_ind[batch_indices, index_y] = pred_y[batch_indices, index_y]
-        f_ind[batch_indices, index_f] = pred_f[batch_indices, index_f]
+        x_ind[batch_indices, index_x] = pred_x[:, max_ids_text:][batch_indices, index_x]
+        y_ind[batch_indices, index_y] = pred_y[:, max_ids_text:][batch_indices, index_y]
+        f_ind[batch_indices, index_f] = pred_f[:, max_ids_text:][batch_indices, index_f]
 
     return x_ind, y_ind, f_ind
 
 
 def entropy(inputs: torch.Tensor):
-    return -torch.sum(torch.exp(inputs) * torch.log2(torch.exp(inputs)), dim=-1)
+    return torch.sum(-torch.exp(inputs) * torch.log2(torch.exp(inputs)), dim=-1)
 
 
 def lowest_entropy(
@@ -208,11 +235,16 @@ def lowest_entropy(
         x_scores, y_scores, f_scores = model(
             ids_text, ids_vis, pos_text, x_ind, y_ind, f_ind, t_types, attn_mask
         )
-        # If there are indices which are already chosen, change their value to 0
         if len(predicted_indices_x) > 0:
-            x_scores[batch_indices, predicted_indices_x] = 0
-            y_scores[batch_indices, predicted_indices_y] = 0
-            f_scores[batch_indices, predicted_indices_f] = 0
+            x_scores[:, max_ids_text:][
+                batch_indices, predicted_indices_x
+            ] = F.log_softmax(torch.ones(1, x_scores.size()[-1]), dim=-1)
+            y_scores[:, max_ids_text:][
+                batch_indices, predicted_indices_y
+            ] = F.log_softmax(torch.ones(1, y_scores.size()[-1]), dim=-1)
+            f_scores[:, max_ids_text:][
+                batch_indices, predicted_indices_f
+            ] = F.log_softmax(torch.ones(1, f_scores.size()[-1]), dim=-1)
 
         # Compute entropies
         entropies_x = entropy(x_scores)
@@ -230,15 +262,15 @@ def lowest_entropy(
         predicted_indices_f.append(index_f.tolist())
 
         # Change the index with the max probability with its prediction
-        x_ind[batch_indices, index_x] = torch.argmax(x_scores, dim=-1)[
-            batch_indices, index_x
-        ]
-        y_ind[batch_indices, index_y] = torch.argmax(y_scores, dim=-1)[
-            batch_indices, index_y
-        ]
-        f_ind[batch_indices, index_f] = torch.argmax(f_scores, dim=-1)[
-            batch_indices, index_f
-        ]
+        x_ind[batch_indices, index_x] = torch.argmax(
+            x_scores[:, max_ids_text:], dim=-1
+        )[batch_indices, index_x]
+        y_ind[batch_indices, index_y] = torch.argmax(
+            y_scores[:, max_ids_text:], dim=-1
+        )[batch_indices, index_y]
+        f_ind[batch_indices, index_f] = torch.argmax(
+            f_scores[:, max_ids_text:], dim=-1
+        )[batch_indices, index_f]
 
     return x_ind, y_ind, f_ind
 
@@ -285,6 +317,10 @@ def generation_strategy_factory(
         )
     elif gen_strategy == "lowest_entropy":
         return lowest_entropy(
+            ids_text, ids_vis, pos_text, x_ind, y_ind, f_ind, t_types, attn_mask, model
+        )
+    elif gen_strategy == "random_discrete":
+        return random_discrete(
             ids_text, ids_vis, pos_text, x_ind, y_ind, f_ind, t_types, attn_mask, model
         )
     else:
